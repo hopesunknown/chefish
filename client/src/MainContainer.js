76ab1b5d7f4aa010a073978349d0e5304bb3db02
Jennifer Tchai <jentchai@gmail.com>
2022-03-16 19:14:21 -0400

    building out some frontend components

---
+++ client/src/MainContainer.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+
+export default function MainContainer({  }) {
+
+  return (
+    <div>
+       {/* <Routes> 
+          <Route exact path="/video" element={
+             } 
+              />
+          <Route exact path="/comments" element={
+          } 
+              />
+          <Route exact path="/comments/:id" element={
+              
+             }
+             />
+          <Route exact path="*" element={
+} 
+            />
+        </Routes>   */}
+    </div>
+  )
+}
